a947f7bd79dc2f803c71c483420a428eaf7286ff
Hun191189 <tranmanhhung191189@gmai.com>
2023-07-12 08:16:11 +0700

    md1 bai7

--- bai7/MD1_7_CaculatorAdvance.js
+++ bai7/MD1_7_CaculatorAdvance.js
@@ -1,3 +1,14 @@
-function dauNgoacTrai(){
-    document.getElementById("screen").innerHTML=("(")
+function bam(value) {
+    let a = document.getElementById("input")
+    a.value += value
+
+}
+function xoa() {
+    document.getElementById("input").value = ("")
+}
+
+function result(){
+    let input = document.getElementById("input")
+    let result = eval(input.value)
+    input.value = result
 }
